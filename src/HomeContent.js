d6aae4591ed6e45807b8a1d0fe64eb70a0de450a
Seth Sutherland <cokolatepal@gmail.com>
2017-07-26 20:08:48 -0600

    solved pic ref problem, working on nav dots

--- src/HomeContent.js
+++ src/HomeContent.js
@@ -1,44 +1,56 @@
 import React, {Component} from 'react';
 
-var imageSrc = "./images/about.jpg";
-
 class HomeContent extends Component {
 
     constructor(){
         super();
 
         this.state = {
-            backgroundPicNum: 4
+            backgroundPicNum: 0,
+            indicatorDot: function(aNum){
+                var theDot = document.getElementsByClassName('dot0');
+
+                console.log(document.getElementsByClassName('dot0'));
+                // theDot.style.backgroundColor = "white";
+            }
         }
-    }
+    };
+
+    componentWillMount(){
+            this.state.indicatorDot(this.state.backgroundPicNum);
+    };
 
     render(){
 
             return(
                 <div className="homeContent">
-                    <img style={{height: "100px", width: '100px'}} src={imageSrc} />
+                    <div className="homeContentHero" style={{ backgroundImage: 'url(' + require( "" + this.props.sections[this.state.backgroundPicNum].picRef + "") + ')' }}>
+                    <div className="homeContentHeroHeading"><h2>{this.props.sections[this.state.backgroundPicNum].title}</h2></div>
+                    </div>
+
+                    <div className="homeContentNext" onClick={()=> {if(this.state.backgroundPicNum < this.props.sections.length - 1){this.setState({backgroundPicNum: this.state.backgroundPicNum + 1})} console.log(this.state.backgroundPicNum);}}><i className="fa fa-long-arrow-right"></i></div>
+                    <div className="homeContentPrevious" onClick={()=> {if(this.state.backgroundPicNum > 0){this.setState({backgroundPicNum: this.state.backgroundPicNum - 1})} console.log(this.state.backgroundPicNum);}}><i className="fa fa-long-arrow-left"></i></div>
+
+                    <div className="homeContentIndicator">
+                    <div className="homeContentIndicatorDot dot0"></div>
+                    <div className="homeContentIndicatorDot dot1"></div>
+                    <div className="homeContentIndicatorDot dot2"></div>
+                    <div className="homeContentIndicatorDot dot3"></div>
+                    <div className="homeContentIndicatorDot dot4"></div>
+                    <div className="homeContentIndicatorDot dot5"></div>
+                    </div>
                 </div>
             )
 
     }
+
+    componentDidMount(){
+
+    }
+
 }
 
 export default HomeContent;
 
 
 
-// {/*<div className="homeContentHero">*/}
-//     {/*<div className="homeContentHeroHeading"><h2>{this.props.sections[this.state.backgroundPicNum].title}</h2></div>*/}
-// {/*</div>*/}
-//
-// {/*<div className="homeContentNext" onClick={()=> {if(this.state.backgroundPicNum < this.props.sections.length){this.state.backgroundPicNum++}; console.log(this.state.backgroundPicNum);}}><i className="fa fa-long-arrow-right"></i></div>*/}
-//     {/*<div className="homeContentPrevious" onClick={()=> {if(this.state.backgroundPicNum > 0){this.state.backgroundPicNum--}; console.log(this.state.backgroundPicNum);}}><i className="fa fa-long-arrow-left"></i></div>*/}
-//
-//     {/*<div className="homeContentIndicator">*/}
-//     {/*<div className="homeContentIndicatorDot dotOne"></div>*/}
-//     {/*<div className="homeContentIndicatorDot dotTwo"></div>*/}
-//     {/*<div className="homeContentIndicatorDot dotThree"></div>*/}
-//     {/*<div className="homeContentIndicatorDot dotFour"></div>*/}
-//     {/*<div className="homeContentIndicatorDot dotFive"></div>*/}
-//     {/*<div className="homeContentIndicatorDot dotSix"></div>*/}
-//     {/*</div>*/}
