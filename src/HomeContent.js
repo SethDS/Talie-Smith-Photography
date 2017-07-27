6939a96869efe100190939c364fd6182e9181b31
Seth Sutherland <cokolatepal@gmail.com>
2017-07-26 18:08:06 -0600

    still working on it.....

--- src/HomeContent.js
+++ src/HomeContent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import './index.css';
+
+var imageSrc = "./images/about.jpg";
 
 class HomeContent extends Component {
 
@@ -7,7 +8,7 @@ class HomeContent extends Component {
         super();
 
         this.state = {
-            backgroundPicNum: 0
+            backgroundPicNum: 4
         }
     }
 
@@ -15,21 +16,7 @@ class HomeContent extends Component {
 
             return(
                 <div className="homeContent">
-                    <div className="homeContentHero">
-                        <div className="homeContentHeroHeading"><h2>{this.props.sections[this.state.backgroundPicNum].title}</h2></div>
-                    </div>
-
-                    <div className="homeContentNext" onClick={()=> {if(this.state.backgroundPicNum < this.props.sections.length){this.state.backgroundPicNum++}; console.log(this.state.backgroundPicNum);}}><i className="fa fa-long-arrow-right"></i></div>
-                    <div className="homeContentPrevious" onClick={()=> {if(this.state.backgroundPicNum > 0){this.state.backgroundPicNum--}; console.log(this.state.backgroundPicNum);}}><i className="fa fa-long-arrow-left"></i></div>
-
-                    <div className="homeContentIndicator">
-                        <div className="homeContentIndicatorDot dotOne"></div>
-                        <div className="homeContentIndicatorDot dotTwo"></div>
-                        <div className="homeContentIndicatorDot dotThree"></div>
-                        <div className="homeContentIndicatorDot dotFour"></div>
-                        <div className="homeContentIndicatorDot dotFive"></div>
-                        <div className="homeContentIndicatorDot dotSix"></div>
-                    </div>
+                    <img style={{height: "100px", width: '100px'}} src={imageSrc} />
                 </div>
             )
 
@@ -37,3 +24,21 @@ class HomeContent extends Component {
 }
 
 export default HomeContent;
+
+
+
+// {/*<div className="homeContentHero">*/}
+//     {/*<div className="homeContentHeroHeading"><h2>{this.props.sections[this.state.backgroundPicNum].title}</h2></div>*/}
+// {/*</div>*/}
+//
+// {/*<div className="homeContentNext" onClick={()=> {if(this.state.backgroundPicNum < this.props.sections.length){this.state.backgroundPicNum++}; console.log(this.state.backgroundPicNum);}}><i className="fa fa-long-arrow-right"></i></div>*/}
+//     {/*<div className="homeContentPrevious" onClick={()=> {if(this.state.backgroundPicNum > 0){this.state.backgroundPicNum--}; console.log(this.state.backgroundPicNum);}}><i className="fa fa-long-arrow-left"></i></div>*/}
+//
+//     {/*<div className="homeContentIndicator">*/}
+//     {/*<div className="homeContentIndicatorDot dotOne"></div>*/}
+//     {/*<div className="homeContentIndicatorDot dotTwo"></div>*/}
+//     {/*<div className="homeContentIndicatorDot dotThree"></div>*/}
+//     {/*<div className="homeContentIndicatorDot dotFour"></div>*/}
+//     {/*<div className="homeContentIndicatorDot dotFive"></div>*/}
+//     {/*<div className="homeContentIndicatorDot dotSix"></div>*/}
+//     {/*</div>*/}
