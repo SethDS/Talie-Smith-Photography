d9d5776db51501b62119eb923c6ebdf442c8787f
Seth Sutherland <cokolatepal@gmail.com>
2017-07-31 14:42:23 -0600

    getting gsap integrated

--- src/HomeContent.js
+++ src/HomeContent.js
@@ -14,54 +14,67 @@ import { TweenMax } from 'gsap';
 
 class HomeContent extends Component {
 
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
-            backgroundPicNum: 0
+            backgroundPicNum: 0,
+            changeHero: function (element) {
+                console.log(element.style);
+
+            }
         }
     };
 
-    componentWillMount(){
 
-    };
 
-    render(){
+        render(){
 
-            return(
+            return (
                 <div className="homeContent">
-                    <div className="homeContentHero" style={{ backgroundImage: 'url(' + require( "" + this.props.sections[this.state.backgroundPicNum].picRef + "") + ')' }}>
-                    <div className="homeContentHeroHeading"><h2>{this.props.sections[this.state.backgroundPicNum].title}</h2></div>
+                    <div className="homeContentHero" ref={(el) => {
+                        this.contentHero = el
+                    }}
+                         style={{backgroundImage: 'url(' + require("" + this.props.sections[this.state.backgroundPicNum].picRef + "") + ')'}}>
+                        <div className="homeContentHeroHeading">
+                            <h2>{this.props.sections[this.state.backgroundPicNum].title}</h2></div>
                     </div>
 
-                    <div className="homeContentNext" onClick={()=> {
-                        if(this.state.backgroundPicNum < this.props.sections.length - 1){this.setState({backgroundPicNum: this.state.backgroundPicNum + 1})}
+                    <div className="homeContentNext" onClick={() => {
+                        this.state.changeHero(this.contentHero);
+                        if (this.state.backgroundPicNum < this.props.sections.length - 1) {
+                            this.setState({backgroundPicNum: this.state.backgroundPicNum + 1})
+                        }
                     }  }>
                         <i className="fa fa-long-arrow-right"></i>
                     </div>
-                    <div className="homeContentPrevious" onClick={()=> {
-                        if(this.state.backgroundPicNum > 0){this.setState({backgroundPicNum: this.state.backgroundPicNum - 1})} }
+                    <div className="homeContentPrevious" onClick={() => {
+                        if (this.state.backgroundPicNum > 0) {
+                            this.setState({backgroundPicNum: this.state.backgroundPicNum - 1})
+                        }
+                    }
                     }>
                         <i className="fa fa-long-arrow-left"></i>
                     </div>
 
                     <div className="homeContentIndicator">
-                            <div className={(this.state.backgroundPicNum!==0?"homeContentIndicatorDot":"theDot")}></div>
-                            <div className={(this.state.backgroundPicNum!==1?"homeContentIndicatorDot":"theDot")}></div>
-                            <div className={(this.state.backgroundPicNum!==2?"homeContentIndicatorDot":"theDot")}></div>
-                            <div className={(this.state.backgroundPicNum!==3?"homeContentIndicatorDot":"theDot")}></div>
-                            <div className={(this.state.backgroundPicNum!==4?"homeContentIndicatorDot":"theDot")}></div>
-                            <div className={(this.state.backgroundPicNum!==5?"homeContentIndicatorDot":"theDot")}></div>
+                        <div
+                            className={(this.state.backgroundPicNum !== 0 ? "homeContentIndicatorDot" : "theDot")}></div>
+                        <div
+                            className={(this.state.backgroundPicNum !== 1 ? "homeContentIndicatorDot" : "theDot")}></div>
+                        <div
+                            className={(this.state.backgroundPicNum !== 2 ? "homeContentIndicatorDot" : "theDot")}></div>
+                        <div
+                            className={(this.state.backgroundPicNum !== 3 ? "homeContentIndicatorDot" : "theDot")}></div>
+                        <div
+                            className={(this.state.backgroundPicNum !== 4 ? "homeContentIndicatorDot" : "theDot")}></div>
+                        <div
+                            className={(this.state.backgroundPicNum !== 5 ? "homeContentIndicatorDot" : "theDot")}></div>
                     </div>
                 </div>
             )
 
-    }
-
-    componentDidMount(){
-
-    }
-
+        }
 }
 
 export default HomeContent;
